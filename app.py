93afa57b2fd693957e39dae8dd4179d03daa8136
Jay3332 <40323796+jay3332@users.noreply.github.com>
2024-05-20 14:33:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, request, Response
 import requests
 app = Flask(__name__)
 BASE = 'https://turbovue-api.jay3332.tech'
+BASE_2 = 'https://api.adapt.chat'
 @app.route('/req', methods=['GET', 'OPTIONS', 'POST'])
 def req():
     res = requests.request(method=request.method, url=BASE + request.args['path'],
+        headers={k: v for k, v in request.headers if k.lower() != 'host'},
+        data=request.get_data(), cookies=request.cookies, allow_redirects=False)
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(k, v) for k, v in res.raw.headers.items() if k.lower() not in excluded_headers]
+    response = Response(res.content, res.status_code, headers)
+    return response
+@app.route('/req2', methods=['GET', 'OPTIONS', 'POST'])
+def req2():
+    res = requests.request(method=request.method, url=BASE2 + request.args['path'],
         headers={k: v for k, v in request.headers if k.lower() != 'host'},
         data=request.get_data(), cookies=request.cookies, allow_redirects=False)
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
